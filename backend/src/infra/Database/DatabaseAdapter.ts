4533899117de749a54d5294a6579d5cba2429722
whyy <icarovsilva1@gmail.com>
2023-01-17 22:50:40 -0300

    feat: create create new habit

--- backend/src/infra/Database/DatabaseAdapter.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-export class DatabaseAdapter {
-  private client: PrismaClient;
-
-  constructor() {
-    this.client = new PrismaClient({ log: ['query'] });
-  }
-}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,39 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import dayjs from 'dayjs';
+
+import { prismaClient } from '../infra/configs/prisma';
 
 export async function router(fastify: FastifyInstance) {
-  fastify.get('/', (req, reply) => {
-    reply.send('<h1>Hello world</h1>');
+  fastify.post('/habits', async (request, reply) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+    const today = dayjs().startOf('day').toDate();
+
+    await prismaClient.habit.create({
+      data: {
+        title,
+        createdAt: today,
+        habitWeekDays: {
+          createMany: {
+            data: weekDays.map((weekDay) => ({ weekDay })),
+          },
+        },
+      },
+    });
+
+    reply.status(201);
+  });
+
+  fastify.get('/dat', (request, reply) => {
+    const getDayParams = z.object({
+      data: z.coerce.date(),
+    });
+
+    const { data } = getDayParams.parse(request.query);
   });
 }
